42a9ca55cfa39bc8303215d192b7c4df73a5f78f
Praveen Sharma <96952903+Praveen0079@users.noreply.github.com>
2025-03-30 23:36:29 +0530

    [Praveen] ADD: Nested Try Catch

--- src/main/java/com/bridgelabz/NestedTryCatch.java
+++ src/main/java/com/bridgelabz/NestedTryCatch.java
@@ -11,7 +11,7 @@ public class NestedTryCatch {
             System.out.print("Enter the number of elements in the array: ");
             int size = sc.nextInt();
             int[] arr = new int[size];
-
+ 
             System.out.println("Enter " + size + " elements:");
             for (int i = 0; i < size; i++) {
                 arr[i] = sc.nextInt();

0b3e9618d36f761196346a6f6fb08dae96d2588b
Praveen Sharma <96952903+Praveen0079@users.noreply.github.com>
2025-03-30 23:33:07 +0530

    [Praveen] ADD: Bank Transaction System

--- src/main/java/com/bridgelabz/BankTransactionSystem.java
+++ src/main/java/com/bridgelabz/BankTransactionSystem.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 import java.util.Scanner;
 
 // Custom Exception for Insufficient Balance
-class InsufficientBalanceException extends Exception {
+class InsufficientBalanceException extends Exception { 
     public InsufficientBalanceException(String message) {
         super(message);
     }
